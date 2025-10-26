72e6d25de33039ab74ffaa44c9f8b7bfe052fafe
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-26 16:07:46 -0600

    Compare dos valores

---
+++ Condicionales/Ejercicio 02 – MayorDeDos/Ejercicio 02 – MayorDeDos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Timers;
+
+namespace Ejercicio_02___MayorDeDos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pide dos números y muestra cuál de los dos es mayor o si son iguales.
+             */
+            Console.Write("Ingresa el primer numero: ");
+            double num1=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Ingresa el segundo numero :");
+            double num2=Convert.ToDouble(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("El numero: " + num1 + " es igual a el numero: " + num2);
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine("El numero: " + num1 + " es mayor que el numero: " + num2);
+            }
+            else {
+                Console.WriteLine("El numero: "+num2+" es mayor que el numero: "+num1);
+            }
+        }
+    }
+}
