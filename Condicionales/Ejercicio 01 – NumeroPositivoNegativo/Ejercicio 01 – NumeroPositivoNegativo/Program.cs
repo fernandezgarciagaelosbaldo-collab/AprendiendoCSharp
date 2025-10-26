2f6345b812a3fd9ecc9bc9123b48948491c43aa5
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-26 15:55:28 -0600

    Primer ejercicio con if-else, para evaluar si un numero es positivo o negativo

---
+++ Condicionales/Ejercicio 01 – NumeroPositivoNegativo/Ejercicio 01 – NumeroPositivoNegativo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Ejercicio_01___NumeroPositivoNegativo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pide al usuario que ingrese un número (puede ser entero o decimal).
+            Luego debes mostrar si ese número es positivo, negativo o cero.*/
+            Console.Write("Ingresa un numero :");
+            
+            double numero=Convert.ToDouble(Console.ReadLine());
+            if (numero <= 0)
+            {
+                Console.WriteLine("Es un numero negativo");
+            }
+            else
+            {
+                Console.WriteLine("Es un numero postivo");
+            }
+
+        }
+    }
+}
