94867b97c07405f41cf818427071c4b3eedea0c6
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-26 21:43:38 -0600

    ejercicio para validar si obtiene un descuento

---
+++ Condicionales/Ejercicio 05 – DescuentoBasico/Ejercicio 05 – DescuentoBasico/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio_05___DescuentoBasico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pide el monto total de una compra.
+            Si el total supera $100, aplica un 10% de descuento y muestra el nuevo total.
+            Si no, muestra el mismo total sin descuento.*/
+            Console.Write("Ingresa el monto total de tu compra :");
+            double compra = Convert.ToDouble(Console.ReadLine());
+            double total;
+            if (compra > 100)
+            {
+                total = compra - (compra * 0.1);
+                Console.WriteLine("La compro fue de " + compra + " pero tuvo el descuento de %10 y el precio final es $" + total);
+
+
+            }
+            else {
+
+                Console.WriteLine("no tienes descuento y te costara $" + compra);
+            }
+        }
+    }
+}
