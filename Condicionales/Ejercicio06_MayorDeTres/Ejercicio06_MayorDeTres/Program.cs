5ac00f46c41aa476088c3dace97ee899be22d9c8
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-27 11:25:49 -0600

    Ejercicio para comparar cual es el mayor de tres de numeros

---
+++ Condicionales/Ejercicio06_MayorDeTres/Ejercicio06_MayorDeTres/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio06_MayorDeTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir tres números y mostrar cuál es el mayor.
+            Console.WriteLine("Ingresa el primer numero :");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo numero :");
+            int numero2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el tercer numero :");
+            int numero3= Convert.ToInt32(Console.ReadLine());
+            if (numero1 > numero2 && numero1 > numero3)
+            {
+                Console.WriteLine("El numero mayor es el " + numero1);
+            }
+            else if (numero2 > numero1 && numero2 > numero3)
+            {
+                Console.WriteLine("El numero mayor es " + numero2);
+            }
+            else {
+                Console.WriteLine("El numero mayor es "+numero3);
+            }
+        }
+    }
+}
