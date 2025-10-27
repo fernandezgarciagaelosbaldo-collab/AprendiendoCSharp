a92e81a153b983b8cf27ec31455e315334b0ea9a
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-26 19:17:30 -0600

    Mayor de edad

---
+++ Condicionales/Ejercicio 04 – MayorDeEdad/Ejercicio 04 – MayorDeEdad/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio_04___MayorDeEdad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pide al usuario su edad (entero) y muestra si es mayor de edad (≥18) o menor de edad.
+            Console.Write("Ingresa tu edad :");
+            int edad =Convert.ToInt32(Console.ReadLine());
+            if (edad >= 18)
+            {
+
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else {
+
+                Console.WriteLine("Es menor de edad");
+            }
+
+        }
+    }
+}
