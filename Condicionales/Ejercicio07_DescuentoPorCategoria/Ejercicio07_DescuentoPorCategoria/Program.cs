bf81366207ba21e798c4d82127bb48f85850a9e7
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-27 11:51:02 -0600

    Apliqué condiciones encadenadas según categorías

---
+++ Condicionales/Ejercicio07_DescuentoPorCategoria/Ejercicio07_DescuentoPorCategoria/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejercicio07_DescuentoPorCategoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir categoría (A, B o C) y aplicar un descuento distinto (A=15%, B=10%, C=5%).
+            Console.Write("Ingresa la categoria :");
+            string categoria =Console.ReadLine();
+            if (categoria == "A")
+            {
+                Console.WriteLine("Elegiste la categoria A y tienes un descuento de" + " 15%");
+            }
+            else if (categoria == "B")
+            {
+                Console.WriteLine("Elegiste la categoria B y tienes un descuento de" + " 10%");
+            }
+            else if (categoria == "C")
+            {
+                Console.WriteLine("Elegiste la categoria C y tienes un descuento de " + "5%");
+            }
+            else {
+                Console.WriteLine("Categoria invalida");
+            }
+
+
+        }
+    }
+}
