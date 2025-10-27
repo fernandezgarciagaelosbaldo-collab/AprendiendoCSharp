68598fdd91fbc1a4a346a1622f208c10b1045219
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-26 18:15:18 -0600

    Numero par e impar

---
+++ Condicionales/Ejercicio 03 – ParOImpar/Ejercicio 03 – ParOImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio_03___ParOImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicita un número entero y determina si es par o impar.
+            Console.Write("Ingresa un numero entero :");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            double numeroProcesado = numero % 2;
+
+            if (numeroProcesado == 0)
+            {
+                Console.WriteLine("El numero " + numero + " es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero "+ numero + " es impar");
+            }
+        }
+    }
+}
