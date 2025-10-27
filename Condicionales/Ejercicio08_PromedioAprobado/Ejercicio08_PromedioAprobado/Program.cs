c4b53f6b27cf563b2c91dd2f5295087e87e5aeab
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-27 17:57:04 -0600

    Calculo promedios y uso operadores lógicos para aprobar/reprobar

---
+++ Condicionales/Ejercicio08_PromedioAprobado/Ejercicio08_PromedioAprobado/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio08_PromedioAprobado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir tres calificaciones, sacar promedio y decir si aprobó (>=6).
+            Console.Write("Ingresa la primer calificacion :");
+            double cal1=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingresa la segunda calificacion :");
+            double cal2=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingresa la tercer calificacion :");
+            double cal3=Convert.ToDouble(Console.ReadLine());   
+            double promedio=(cal1+cal2+cal3)/3;
+            if (promedio >= 6)
+            {
+                Console.WriteLine("Felicidades pasate la materia , con un promedio de " + promedio);
+            }
+            else {
+                Console.WriteLine("No aprobaste :( , tu promedio fue de "+promedio);
+            }
+        }
+    }
+}
