16740bf45f5c47a2e20b31f3e5dc86716911f0f1
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-27 18:55:30 -0600

    Determinación de tipo de triángulo con condiciones

---
+++ Condicionales/Ejercicio12_TipoDeTriang/Ejercicio12_TipoDeTriang/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Ejercicio12_TipoDeTriang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir lados y decir si es equilátero, isósceles o escaleno.
+            Console.WriteLine("Ingresa el primer lado de el triangulo :");
+            double lado1=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo lado de el triangulo :");
+            double lado2=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa el tercer lado de el triangulo :");
+            double lado3=Convert.ToDouble(Console.ReadLine());
+            if (lado1 == lado2 && lado1 == lado3)
+            {
+                Console.WriteLine("Es un triangulo Equilátero");
+            }
+            else
+            {
+                if (lado1 == lado2 && lado1 != lado3)
+                {
+                    Console.WriteLine("Es un triangulo Isósceles");
+                }
+                else {
+                    if (lado1 != lado2 && lado1 == lado3)
+                    {
+                        Console.WriteLine("Es un triangulo Isósceles");
+                    }
+                    else {
+                        Console.WriteLine("Es un triangulo Escaleno");
+                    }
+
+                
+                }
+            }
+        }
+    }
+}
