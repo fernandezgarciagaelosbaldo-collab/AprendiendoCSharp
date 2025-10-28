a360f227d93da847fa827e64e34599762e776189
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-27 19:05:03 -0600

    Combiné condiciones de tipo y valor

---
+++ Condicionales/Ejercicio13_EdadYGener/Ejercicio13_EdadYGener/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ejercicio13_EdadYGener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir edad y género y dar un mensaje personalizado (ej. "Eres mayor de edad, Sr.").
+            Console.Write("holaa, ingresa tu edad :");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa tu genero ,masculino o femenino?");
+            string genero = Console.ReadLine();
+            if (edad >= 60 && genero == "masculino")
+            {
+                Console.WriteLine("Eres mayor de edad, Sr.");
+            }
+            else if (genero == "masculino")
+            {
+                Console.WriteLine("Hola Sr");
+
+            }
+            else if (edad >= 60 && genero == "femenino")
+            {
+                Console.WriteLine("Eres mayor de edad, Sra.");
+            }
+            else {
+                Console.WriteLine( "holaaa , Sra");
+            }
+        
+
+        }
+    }
+}
