34b841d2fba5bfff917951b6ff28728a0d86fc24
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-30 16:31:51 -0600

    Implemente el switch para hacer validaciones de operaciones en una calculadora

---
+++ Condicionales/Ejercicio18_CalculadoraSimple/Ejercicio18_CalculadoraSimple/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Ejercicio18_CalculadoraSimple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir dos números y una operación(+, -, *, /) y mostrar el resultado.
+            Console.Write("Ingresa el primer numero :");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa el segundo numero :");
+            int num2=Convert.ToInt32(Console.ReadLine());
+            Console.Write("que operacion quieres hacer con los numeros (+, -, *, /) :");
+            string operacion=Console.ReadLine();
+            double resultado = 0;
+            switch (operacion) {
+                case "+":
+                    resultado=num1 + num2;
+                    Console.WriteLine("El resultado de la suma es de "+resultado);
+                    break;
+                    case "-":
+                    resultado=num1 - num2;
+                    Console.WriteLine("El resultado de la resta es de " + resultado);
+                    break;
+                    case "*":
+                    resultado=num1 * num2;
+                    Console.WriteLine("El resultado de la multiplicacion es de " + resultado);
+                    break;
+                    case "/":
+                    resultado=num1 / num2;
+                    Console.WriteLine("El resultado de la divicion es de " + resultado);
+                    break;
+                default:
+                    Console.WriteLine("Simbolo no valido");
+                    break;
+            }
+        }
+    }
+}
