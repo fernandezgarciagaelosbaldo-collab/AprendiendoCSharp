21bab257374880d6bc651548deb41a458f18bae6
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-30 16:20:11 -0600

    Apliqué switch con múltiples casos para mostrar meses

---
+++ Condicionales/Ejercicio17_MesesDelAnio/Ejercicio17_MesesDelAnio/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Ejercicio17_MesesDelAnio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un número (1–12) y mostrar el nombre del mes.
+            Console.Write("Ingresa un numero de el 1 a el 12 :");
+            int mes =Convert.ToInt32(Console.ReadLine());
+            switch (mes) { 
+             case 1:
+                    Console.WriteLine("Enero");
+                    break;
+                    case 2:
+                    Console.WriteLine("Febrero");
+                    break;
+                    case 3:
+                    Console.WriteLine("Marzo");
+                    break;
+                case 4:
+                    Console.WriteLine("Abril");
+                    break;
+                    case 5:
+                    Console.WriteLine("Mayo");
+                    break;
+                    case 6:
+                    Console.WriteLine("Junio");
+                    break;
+                    case 7:
+                    Console.WriteLine("Julio");
+                    break;
+                    case 8:
+                    Console.WriteLine("Agosto");
+                    break;
+                    case 9:
+                    Console.WriteLine("Septiembre");
+                    break;
+                    case 10:
+                    Console.WriteLine("Octubre");
+                    break;
+                    case 11:
+                    Console.WriteLine("Noviembre");
+                    break;
+                    case 12:
+                    Console.WriteLine("Diciembre");
+                    break;
+                    default:
+                    Console.WriteLine("Numero no valido");
+                    break;
+            
+            }
+        }
+    }
+}
