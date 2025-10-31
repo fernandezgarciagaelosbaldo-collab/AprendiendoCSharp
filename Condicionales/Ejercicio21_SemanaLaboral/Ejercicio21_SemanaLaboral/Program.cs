2fb512132c04953599b7917286df809d649bc70c
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-10-31 17:48:37 -0600

    Utilize un switch para validar si es fin de semana o dia laboral

---
+++ Condicionales/Ejercicio21_SemanaLaboral/Ejercicio21_SemanaLaboral/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+
+namespace Ejercicio21_SemanaLaboral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un día y decir si es laboral o fin de semana.
+            Console.WriteLine("Ingresa un dia que quieras saber si es laboral o no , ejemplo lunes - dia laboral");
+            string dia=Console.ReadLine();
+            switch (dia) {
+                case "lunes":
+                    Console.WriteLine("Dia laboral");
+                    break;
+                case "martes":
+                    Console.WriteLine("Dia laboral");
+                    break;
+                case "miercoles":
+                    Console.WriteLine("Dia laboral");
+                    break;
+                case "jueves":
+                    Console.WriteLine("Dia laboral");
+                    break;
+                case "viernes":
+                    Console.WriteLine("Dia laboral");
+                        break;
+                case "sabado":
+                    Console.WriteLine("Fin de semana");
+                    break;
+                case "domingo":
+                    Console.WriteLine("Fin de semana");
+                    break;
+                default:
+                    Console.WriteLine("dia no valido");
+                    break;
+
+
+
+            }
+        }
+    }
+}
