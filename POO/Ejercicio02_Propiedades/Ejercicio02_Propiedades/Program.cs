c9f3d853569ce44a382ef145ba4b58e4efe4108d
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-02 10:43:49 -0600

    Usé propiedades para encapsular los atributos de una clase

---
+++ POO/Ejercicio02_Propiedades/Ejercicio02_Propiedades/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicio02_Propiedades
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementar propiedades con get y set en la clase Persona.
+            Persona persona1 = new Persona();
+            persona1.Edad = 19;
+            persona1.Nombre = "Gael";
+            Console.WriteLine($"Hola soy {persona1.Nombre} y tengo {persona1.Edad} años de edad");
+
+        }
+    }
+}
