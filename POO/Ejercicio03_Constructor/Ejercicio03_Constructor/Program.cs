385176338232eed9a5a0861bfc7cf757ca335831
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-02 10:53:41 -0600

    Implementé constructores para inicializar objetos fácilmente

---
+++ POO/Ejercicio03_Constructor/Ejercicio03_Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio03_Constructor
+{
+    public class Persona {
+        public string Nombre {  get; set; }
+        public int Edad { get; set; }
+        public Persona(string nombre,int edad) { 
+        Nombre = nombre;
+            Edad = edad;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //añade un constructor con parámetros para inicializar Nombre y Edad al crear el objeto.
+            Persona persona1 =new Persona("Gael",19);
+            Persona persona2 = new Persona("Alejandro",20);
+            Console.WriteLine($"Hola soy {persona1.Nombre} y tengo {persona1.Edad}");
+            Console.WriteLine($"Mi amigo se llama {persona2.Nombre} y tiene {persona2.Edad} años ");
+        }
+    }
+}
