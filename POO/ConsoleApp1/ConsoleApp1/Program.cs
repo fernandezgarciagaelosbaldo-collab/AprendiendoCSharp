ed17c21b48df6d72d73d336435998f0a57817e15
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-02 10:01:16 -0600

    añadi una nueva carpeta y cree mi primera clase

---
+++ POO/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public Persona(string nombre,int edad) {
+            Nombre= nombre;
+            Edad= edad;
+        }
+
+    }
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Crear una clase Persona con nombre y edad, y mostrar los datos.
+            Persona persona1 = new Persona("Gael",19);
+            Console.WriteLine($"Holaa {persona1.Nombre} tienes {persona1.Edad} años");
+
+        }
+    }
+}
