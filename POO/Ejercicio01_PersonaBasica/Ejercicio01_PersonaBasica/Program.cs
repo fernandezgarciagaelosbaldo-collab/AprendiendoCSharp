6dd974436415ff80474199c3603078a267df39e0
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-02 10:32:08 -0600

    Aprendí a crear e instanciar mi primera clase en C#

---
+++ POO/Ejercicio01_PersonaBasica/Ejercicio01_PersonaBasica/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ejercicio01_PersonaBasica
+{
+    public class Persona {
+       public string Nombre;
+        public int Edad;
+    
+    }
+    internal class Program
+       
+    {
+        static void Main(string[] args)
+        {
+            //	Crear una clase Persona con nombre y edad, y mostrar los datos.
+            Persona persona1 = new Persona();
+            persona1.Nombre = "Gael";
+            persona1.Edad = 19;
+            Console.WriteLine($"Hola soy {persona1.Nombre} y tengo {persona1.Edad} años");
+
+        }
+    }
+}
