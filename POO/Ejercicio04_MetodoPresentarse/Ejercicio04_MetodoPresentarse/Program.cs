fc914ebca30f3594d53a285860be2a8144f6950f
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-02 19:59:19 -0600

    Aprendí a usar métodos dentro de una clase

---
+++ POO/Ejercicio04_MetodoPresentarse/Ejercicio04_MetodoPresentarse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ejercicio04_MetodoPresentarse
+{
+    public class Persona {
+        public string Nombre {  get; set; }
+        public int Edad {  get; set; }
+        public Persona(string nombre, int edad) {
+            Nombre= nombre;
+            Edad= edad;
+
+        }
+        public void presentarse()
+        {
+            Console.WriteLine($"Hola {Nombre} tienes {Edad} años");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*agrega al Persona un método Presentarse() 
+             * que imprime un mensaje usando sus propiedades;
+             * crea varias instancias y llama al método.*/
+            Persona persona1 = new Persona("Gael",23);
+            Persona persona2 = new Persona("Pepe", 34);
+            Persona persona3 = new Persona("osbaldo",34);
+            persona1.presentarse();
+            persona2.presentarse();
+            persona3.presentarse();
+        }
+    }
+}
