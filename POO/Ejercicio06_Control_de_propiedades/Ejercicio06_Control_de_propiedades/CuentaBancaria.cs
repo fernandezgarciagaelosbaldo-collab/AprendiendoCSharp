7925b43bf7e5391cbb6e9f30cc6f82a0e716b804
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-04 18:31:46 -0600

    añadir clase CuentaBancaria con encapsulamiento y validaciones básicas

---
+++ POO/Ejercicio06_Control_de_propiedades/Ejercicio06_Control_de_propiedades/CuentaBancaria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio06_Control_de_propiedades
+{
+    public class CuentaBancaria
+    {
+        /*Un campo privado saldo.
+         Una propiedad pública Saldo que:
+         Solo permita modificar el saldo si el nuevo valor es mayor o igual que 0.
+         Si intentan poner un valor negativo, simplemente no cambia y muestra un mensaje de advertencia.
+         Un método público Depositar(double cantidad) que aumente el saldo si cantidad > 0.
+         Un método público Retirar(double cantidad) que reste del saldo si hay suficiente dinero.*/
+        private double saldo;
+        public double Saldo { get { return saldo; } private set { if (value >= 0) { saldo = value; } 
+                else { Console.WriteLine("No la puedes cambiar por nuemeros negativos"); } } }
+
+        public void  Depositar (double cantidad) {
+            if (cantidad > 0)
+            {
+                saldo += cantidad;
+            }
+            else { Console.WriteLine("No puedes depositar numeros negativos"); }
+        }
+        public void Retirar(double cantidad)
+        {
+            if (cantidad < saldo)
+            {
+                saldo -= cantidad;
+
+            }
+            else { Console.WriteLine("No tienes suficientes fondos"); }
+        }
+    }
+}
---
+++ POO/Ejercicio06_Control_de_propiedades/Ejercicio06_Control_de_propiedades/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio06_Control_de_propiedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crea una cuenta con un saldo inicial.
+              Prueba varias operaciones válidas e inválidas (por ejemplo, retirar más de lo que hay o depositar negativo)
+              Muestra el resultado con Console.WriteLine.*/
+            CuentaBancaria cuenta = new CuentaBancaria();
+            Console.WriteLine("Tienes de saldo "+cuenta.Saldo);
+            cuenta.Depositar(1000000);
+            if(cuenta.Saldo > 100000)
+            {
+                Console.WriteLine("Felicidades tu saldo total es de "+cuenta.Saldo);
+            }
+            cuenta.Retirar(12232121);
+            cuenta.Retirar(1223);
+            cuenta.Depositar(-23);
+            Console.WriteLine("Tu nuevo saldo es de "+cuenta.Saldo);
+        }
+    }
+}
