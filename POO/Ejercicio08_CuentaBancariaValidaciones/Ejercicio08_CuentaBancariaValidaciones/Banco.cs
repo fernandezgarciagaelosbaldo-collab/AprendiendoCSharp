fd1ee0c062710cd956999ac7210ecb6a98c4f70a
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-12 18:16:25 -0600

    Apliqué validaciones internas y control de acceso a datos

---
+++ POO/Ejercicio08_CuentaBancariaValidaciones/Ejercicio08_CuentaBancariaValidaciones/Banco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio08_CuentaBancariaValidaciones
+{
+    //Ampliar CuentaBancaria: agregar Depositar, Retirar, y mostrar mensajes de error si no hay saldo.
+    class cuentaBancaria
+    {
+        private double montoTotal=0;
+        public double MontoTotal
+        {
+            get { return montoTotal; }
+             private set
+            {
+                if (value >= 0) { montoTotal = value; }
+                else { Console.WriteLine("el numero no puede ser 0 "); }
+            }
+        }
+        public void Depositar(double Monto) {
+           
+            MontoTotal += Monto;
+            Console.WriteLine($"Listo se agregaron {Monto} y ahora tienes un total de {MontoTotal}");
+        }
+        public void Retirar(double Retirar)
+        {
+            if (MontoTotal >= Retirar) {
+                MontoTotal -= Retirar;
+                Console.WriteLine("Perfecto , ahora solo tienes " + MontoTotal+" pesos");
+            }
+            else { Console.WriteLine("No tienes suficientes fondos en la cuenta "); }
+        }
+        
+    }
+}
---
+++ POO/Ejercicio08_CuentaBancariaValidaciones/Ejercicio08_CuentaBancariaValidaciones/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ejercicio08_CuentaBancariaValidaciones
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            cuentaBancaria cuentaBancaria1=new cuentaBancaria();
+            Console.WriteLine("Que quieres hacer 1-Depositar,2-Retirar");
+            int elegcion =Convert.ToInt32(Console.ReadLine());
+            switch (elegcion){
+              case 1:
+                    Console.Write("Cuanto quieres depositar ?");
+                    double deposito = Convert.ToDouble(Console.ReadLine());
+                    if (deposito > 0)
+                    {
+                        cuentaBancaria1.Depositar(deposito);
+                    }
+                    else {
+                        Console.WriteLine("Tines que depositar una cantidad mayo a 0");
+                    }
+                        break;
+                    case 2:
+                     Console.Write("Cuanto quieres Retirar :");
+                     double retiro = Convert.ToDouble(Console.ReadLine());
+                    if (retiro > cuentaBancaria1.MontoTotal)
+                    {
+                        Console.WriteLine("No se puede retirar mas de lo que tiene ");
+                    }
+                    else {
+                     cuentaBancaria1.Retirar(retiro);
+
+                    }
+                    break;
+
+
+            }
+
+        }
+    }
+}
