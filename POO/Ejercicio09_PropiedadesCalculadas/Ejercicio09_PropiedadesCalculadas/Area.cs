2e5cb7c7071cae630cacd6d2435c962ca450f8e2
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-12 18:36:33 -0600

    Aprendí a crear propiedades calculadas

---
+++ POO/Ejercicio09_PropiedadesCalculadas/Ejercicio09_PropiedadesCalculadas/Area.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio09_PropiedadesCalculadas
+{
+    //Clase Rectangulo con Base y Altura, y una propiedad calculada Area (solo lectura).
+    public class Area
+    {
+        public double Base {  get; set; } 
+        public double Altura { get; set; }
+        public Area (double basee,double altura)
+        {
+            Base = basee;
+            Altura = altura;
+        }
+        public double Areaa => Base * Altura;
+        public void mostrar()
+        {
+            Console.WriteLine("El area es de "+ Areaa);
+        }
+    }
+}
---
+++ POO/Ejercicio09_PropiedadesCalculadas/Ejercicio09_PropiedadesCalculadas/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio09_PropiedadesCalculadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Area rectangulo1 = new Area(89,34);
+            rectangulo1.mostrar();
+
+        }
+    }
+}
