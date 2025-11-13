a5cbcc957e408adfea2747ca1a856805a762aef4
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-12 18:53:08 -0600

    Implementé un contador de objetos con miembros estáticos

---
+++ POO/Ejercicio10_ContadorObjetos/Ejercicio10_ContadorObjetos/Contador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10_ContadorObjetos
+{
+    public class Usuario
+    {
+        //Clase Usuario con campo estático para contar cuántos objetos se crean.
+        public string Nombre;
+        public int Edad;
+        public static int contador;
+        public Usuario(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            contador++;
+        }
+        public void mostrarcontador()
+        {
+            Console.WriteLine(contador);
+        }
+
+
+    }
+}
---
+++ POO/Ejercicio10_ContadorObjetos/Ejercicio10_ContadorObjetos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio10_ContadorObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario persona1 = new Usuario("Gael",19);
+            Usuario persona2 = new Usuario("Alan", 19);
+            Usuario persona3 = new Usuario("Alejandro", 19);
+            Usuario persona4 = new Usuario("Karol", 19);
+            Usuario persona5 = new Usuario("Mayte", 19);
+            persona5.mostrarcontador();
+            persona3.mostrarcontador();
+        }
+    }
+}
