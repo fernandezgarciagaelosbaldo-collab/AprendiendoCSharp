df55d6f5c3f336e22b5cc5721a7fe814a2bc0a21
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-15 14:19:38 -0600

    Usé interfaces para definir comportamientos comunes

---
+++ POO/Ejercicio19_PolimorfismoInterfaz/Ejercicio19_PolimorfismoInterfaz/Comportamiento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19_PolimorfismoInterfaz
+{
+    public interface Sonido{
+        void sonido();
+    }
+    public class Comportamiento:Sonido
+    {
+        public void sonido()
+        {
+            Console.WriteLine("Holaaaaaa");
+        }
+    }
+    public class Perro : Sonido
+    {
+        public void sonido()
+        {
+            Console.WriteLine("Guau guau");
+        }
+    }
+    public class Carro:Sonido
+    {
+        public void sonido()
+        {
+            Console.WriteLine("Sonido de arrancon");
+        }
+
+    }
+}
---
+++ POO/Ejercicio19_PolimorfismoInterfaz/Ejercicio19_PolimorfismoInterfaz/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio19_PolimorfismoInterfaz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sonido> Sonidos=new List<Sonido>();
+            Sonidos.Add(new Comportamiento());
+            Sonidos.Add(new Perro());
+            Sonidos.Add(new Carro());
+
+            foreach(Sonido i in Sonidos)
+            {
+                i.sonido();
+            }
+        }
+    }
+}
