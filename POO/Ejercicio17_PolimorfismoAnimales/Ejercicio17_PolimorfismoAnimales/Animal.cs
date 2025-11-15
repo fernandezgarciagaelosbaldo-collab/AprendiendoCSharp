4b890d921eafa6fcb56df6202ffdb47d297cf13a
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-15 13:17:10 -0600

    Polimorfismo con listas de objetos

---
+++ POO/Ejercicio17_PolimorfismoAnimales/Ejercicio17_PolimorfismoAnimales/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17_PolimorfismoAnimales
+{
+    public class Animal
+    {
+        public virtual void Sonido()
+        {
+            Console.WriteLine("..");
+        }
+    }
+    public class Tigre : Animal
+    {
+        public override void Sonido()
+        {
+            Console.WriteLine("GRRRR");
+        }
+    }
+    public class Perro : Animal {
+        public override void Sonido() {
+            Console.WriteLine("Guau guau");
+        }
+    }
+    public class Gato : Animal
+    {
+        public override void Sonido()
+        {
+            Console.WriteLine("Miau miau ");
+        }
+    }
+}
---
+++ POO/Ejercicio17_PolimorfismoAnimales/Ejercicio17_PolimorfismoAnimales/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio17_PolimorfismoAnimales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> Animales = new List<Animal>();
+            Animales.Add(new Tigre());
+            Animales.Add(new Perro());
+            Animales.Add(new Gato());
+            foreach(Animal i in Animales)
+            {
+                i.Sonido();
+            }
+           
+        }
+    }
+}
