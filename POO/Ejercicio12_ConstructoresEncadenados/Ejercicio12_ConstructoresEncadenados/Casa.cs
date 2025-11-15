78487be21608236e4a2752f4de2c0d6c35bb7bb7
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-14 21:00:32 -0600

    Encadené constructores usando this()

---
+++ POO/Ejercicio12_ConstructoresEncadenados/Ejercicio12_ConstructoresEncadenados/Casa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12_ConstructoresEncadenados
+{
+    public class Casa
+    {
+        public string Color {  get; set; }
+        public int Numero { get; set; }
+        public double Valor {  get; set; }
+        public string Dueno { get; set; } 
+        public Casa(string dueno,string color ,int numero, double valor ) {
+         Dueno = dueno;
+            Color = color;
+            Numero = numero;
+            Valor = valor;
+        }
+        public Casa(string dueno,string color,int numero):this(dueno,color,numero,1000000) {
+        }
+        public Casa(string dueno, string color) : this(dueno, color, 123)
+        {
+        }
+        public Casa(string dueno):this(dueno,"Rojo") { }
+        public Casa():this("Gael") { }
+        public void Mostrar()
+        {
+            Console.WriteLine($"Dueño: {Dueno}, Color: {Color}, Número: {Numero}, Valor: {Valor}");
+        }
+
+    }
+}
---
+++ POO/Ejercicio12_ConstructoresEncadenados/Ejercicio12_ConstructoresEncadenados/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ejercicio12_ConstructoresEncadenados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Casa casa1 = new Casa();
+            casa1.Mostrar(); 
+            Casa casa2 = new Casa("Lucía");
+            casa2.Mostrar(); 
+            Casa casa3 = new Casa("Pedro", "Azul");
+            casa3.Mostrar();
+            Casa casa4 = new Casa("Ana", "Verde", 456);
+            casa4.Mostrar(); 
+            Casa casa5 = new Casa("Carlos", "Blanco", 789, 2500000);
+            casa5.Mostrar();
+        }
+    }
+}
