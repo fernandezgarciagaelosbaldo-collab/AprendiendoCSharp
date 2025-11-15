b9e5ab37eb68ad0863f2b4655363dadb231d5472
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-14 20:30:03 -0600

    Use sobrecarga de constructores para inicializar objetos de diferentes formas

---
+++ POO/Ejercicio11_ToString/Ejercicio11_ToString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio11_ToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SobreEscritura persona1 = new SobreEscritura();
+            SobreEscritura persona2 = new SobreEscritura("Gael",23);
+            SobreEscritura persona3 = new SobreEscritura(persona2);
+            persona1.Mostrar();
+            persona2.Mostrar();
+            persona3.Mostrar();
+
+        }
+    }
+}
---
+++ POO/Ejercicio11_ToString/Ejercicio11_ToString/SobreEscritura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11_ToString
+{
+    public class SobreEscritura
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public SobreEscritura() {
+            Nombre = "";
+            Edad = 0;
+        }
+        public SobreEscritura(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+        public SobreEscritura(SobreEscritura copia)
+        {
+            Nombre = copia.Nombre;
+            Edad = copia.Edad;
+        }
+        public void Mostrar() {
+            Console.WriteLine($"Holaaa {Nombre} tienes {Edad} años");
+        }
+
+    }
+}
