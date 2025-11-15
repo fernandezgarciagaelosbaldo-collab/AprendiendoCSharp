4ab2a5240d2ca8d37fd2855f1b8d9d71995cb782
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-15 13:47:12 -0600

    Aprendi a usar abstract y como utilizarlo en las clases hijas

---
+++ POO/Ejercicio18_ClaseAbstracta/Ejercicio18_ClaseAbstracta/Area.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio18_ClaseAbstracta
+{
+    public abstract class Area
+    {
+        public abstract double CalcularArea();
+    }
+
+    public class Tringulo : Area { 
+     public double Base {  get; set; }
+        public double Altura {  get; set; }
+        public Tringulo(double basee,double altura)
+        {
+            Base=basee;
+            Altura=altura;
+
+        }
+        public override double CalcularArea() {
+            return (Base * Altura) / 2;
+        }
+    }
+    public class Reactangulo:Area
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+        public Reactangulo(double basee,double altura)
+        {
+            Base=basee;
+            Altura=altura;
+        }
+        public override double CalcularArea()
+        {
+            return (Base * Altura);
+        }
+
+    }
+    public class Cuadrado:Area
+    {
+        public double Lado { get; set; }
+        public Cuadrado(double lado) 
+        {
+        Lado=lado;
+        }
+        public override double CalcularArea()
+        {
+            return Lado*Lado;
+        }
+    }
+}
---
+++ POO/Ejercicio18_ClaseAbstracta/Ejercicio18_ClaseAbstracta/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio18_ClaseAbstracta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Area> Figuras=new List<Area>();
+            Figuras.Add(new Tringulo(23,34));
+            Figuras.Add(new Reactangulo(23, 34));
+            Figuras.Add(new Cuadrado(23));
+            foreach (Area area in Figuras) {
+                Console.WriteLine(area.CalcularArea());
+            }
+        }
+    }
+}
