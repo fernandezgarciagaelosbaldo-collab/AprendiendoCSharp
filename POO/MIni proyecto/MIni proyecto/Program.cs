8d67c093928b0eb49e3b802b615f9efb2da6beb3
GaelOFG2006 <fernandezgarciagaelosbaldo@gmail.com>
2025-11-15 20:31:51 -0600

    Creacion de clases , Producto,Provedor y Orden de compra, queda pendiente el program.cs

---
+++ POO/MIni proyecto/MIni proyecto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MIni_proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
